7a9ef179a1ca6fb2805b059f04a3b728c62d9bf5
littlegirlRachel <15033592279@163.com>
2018-01-17 18:38:04 +0800

    finish basic building

---
+++ src/component/ImgFigure.js
@@ -0,0 +1,15 @@
+// import React, { Component } from 'react';
+//
+// class ImgFigure extends Component{
+//     render(){
+//         return(
+//           <figure>
+//               <img src="" alt=""/>
+//               <figcaption>
+//                   <h2></h2>
+//               </figcaption>
+//           </figure>
+//         );
+//     }
+// }
+// export default ImgFigure;
