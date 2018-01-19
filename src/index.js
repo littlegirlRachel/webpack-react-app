dd2bfcfe87644a97de7080a549e1b7acb75f16f5
littlegirlRachel <15033592279@163.com>
2018-01-19 11:14:22 +0800

    complete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
+import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/Main';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Render the main component into the dom
+ReactDOM.render(<App/>, document.getElementById('app'));
